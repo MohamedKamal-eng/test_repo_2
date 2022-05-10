ce74e48cc794711d5596d5765ca3ed2e88d4d997
MohamedKamal <mohamedkamal.2996@gmail.com>
2022-05-10 20:18:32 +0200

    first commit

---
+++ 01-DIO_Basics/main.c
@@ -0,0 +1,26 @@
+/*
+ * main.c
+ *
+ *  Created on: May 10, 2022
+ *      Author: THE LAPTOP SHOP
+ */
+#include<avr/io.h>
+#include<util/delay.h>
+int main(void)
+{
+	DDRA=0b00001111; //PA0 Output
+
+	while(1) //Super loop
+	{
+		//PORTA=0b00000001; //PA0A HIGH
+	  //  _delay_ms(1000);
+	  //  PORTA=0;
+	  //  _delay_ms(1000);
+		PORTA=0b00000001;
+		for(int i=0;i<4;i++)
+    {
+		_delay_ms(250);
+		PORTA<<=1;
+	}
+	}
+}
